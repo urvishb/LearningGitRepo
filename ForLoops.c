a1040409bd90f16bc34eb28ee0e53c3cd0d6b175
Urvish <ubundela55@gmail.com>
2020-10-04 13:28:02 +0530

    Updated for loops with user input

--- ForLoops.c
+++ ForLoops.c
@@ -2,12 +2,16 @@
 
 int main()
 {
-	int n = 12;		
+	int n = 12;
+	
+	printf("Enter any value:");	
+	scanf("%d", &n);	
 	for (int i = 1 ; i <= n ;  i++)
 	{
 		printf("Counting %d\n", i);
 	}
 	
-	printf("Counting Ends Here!\n")
+	printf("Counting Ends Here!\n");
+	
 	return 0;
 }
